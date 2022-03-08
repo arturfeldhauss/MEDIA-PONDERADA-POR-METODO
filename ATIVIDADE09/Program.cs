7b1dcdc5d15d0c583d08b3e977c7127ef3dcb138
artur <artur@LAPTOP-T9V5DGN8>
2022-03-08 16:15:28 -0300

    Add project files.

---
+++ ATIVIDADE09/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ATIVIDADE09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            mediaponderada media = new mediaponderada();
+            media.peco1 = 4;
+            media.peco2 = 6;
+            media.nota1 = 3;
+            media.nota2 = 5;
+            Console.WriteLine("A media ponderada é: " + media.mediap());
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ATIVIDADE09/mediaponderada.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATIVIDADE09
+{
+    public class mediaponderada
+    {
+        public int peco1;
+        public int peco2;
+        public int nota1;
+        public int nota2;
+
+        public double mediap()
+        {
+            int mediap = (peco1 * nota1 + peco2 * nota2) / (peco2 + peco1);
+            return mediap;
+        
+        }
+        
+
+
+    
+    }
+}
